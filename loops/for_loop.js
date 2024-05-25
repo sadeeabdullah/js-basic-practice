2a651a4f14ba34af3857559f279cfa26acff70c1
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-05-25 07:38:20 +0600

    started the basic of js for more proficienc on js

---
+++ loops/for_loop.js
@@ -0,0 +1,4 @@
+const namelist = ["John", "Jane", "Jack", "Jill"];
+for(let i = 0; i<namelist.length; i++){
+    console.log(namelist[i].toUpperCase());
+}
---
+++ loops/while_lop.js
@@ -0,0 +1,6 @@
+const namelist=["John", "Jane", "Jack", "Jill"];
+let i=0;
+while(i<namelist.length){
+    console.log(namelist[i].toUpperCase());
+    i++;
+}
