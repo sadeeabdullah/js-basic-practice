831f596857db92ad9c8386b56e06635518d61ee7
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-06-01 19:26:47 +0600

    added the for loop for printing 1 to 1000

--- loops/for_loop.js
+++ loops/for_loop.js
@@ -2,3 +2,9 @@ const namelist = ["John", "Jane", "Jack", "Jill"];
 for(let i = 0; i<namelist.length; i++){
     console.log(namelist[i].toUpperCase());
 }
+
+//printing 1000 in js
+
+for( let j = 1; i<1001; i++){
+    console.log(j);
+}
