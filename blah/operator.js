afff0324e4d71f3341ca9f0476ea07cceb21ad66
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-06-01 17:47:14 +0600

    adding the operators

---
+++ blah/operator.js
@@ -0,0 +1,13 @@
+console.log("operator in js")
+
+const a= 15;
+const b = 4;
+// operator in js
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b);
+// console.log("a++=",a++);
+// console.log("a--=",a--);
