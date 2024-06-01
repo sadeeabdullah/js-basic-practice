af7561897acf68af6dc01c21d5f7b5a55ccdbffc
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-06-01 19:51:51 +0600

    added the factorial retrieving loop

--- loops/for_loop.js
+++ loops/for_loop.js
@@ -5,6 +5,24 @@ for(let i = 0; i<namelist.length; i++){
 
 //printing 1000 in js
 
-for( let j = 1; i<1001; i++){
+for( let j = 1; j<1001; j++){
     console.log(j);
 }
+
+// first n number of numbers summation
+
+let sum = 0;
+let n = 7;
+for( let l = 0; l<n; l++){
+    sum += (l+1) // or sum = sum + (l+1)
+}
+console.log(sum)
+
+// first n number of numbers multiplication or factorial
+
+let mul = 1;
+let m = 7;
+for( let k = 0; k<m; k++){
+    mul *= (k+1) // or mul = mul * (k+1)
+}
+console.log(mul)
