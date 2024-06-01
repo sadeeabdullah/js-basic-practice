cf9826653af14222c60577b113bc5dfc905caa88
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-06-01 17:54:33 +0600

    added increament and decreament operators

--- blah/operator.js
+++ blah/operator.js
@@ -1,13 +1,15 @@
 console.log("operator in js")
 
-const a= 15;
-const b = 4;
+let a= 15;
+let b = 4;
 // operator in js
-console.log("a+b=",a+b);
-console.log("a-b=",a-b);
-console.log("a*b=",a*b);
-console.log("a/b=",a/b);
-console.log("a%b=",a%b);
-console.log("a**b=",a**b);
-// console.log("a++=",a++);
-// console.log("a--=",a--);
+console.log("a+b=",a+b); //addition
+console.log("a-b=",a-b); //subtraction
+console.log("a*b=",a*b); //multiplication
+console.log("a/b=",a/b);  //quotient
+console.log("a%b=",a%b); //remainder
+console.log("a**b=",a**b); //a to the power of b
+console.log("a++=",a++);  //post increment will increase later and console the previous value
+console.log("a--=",a--); //post decreamnet will decrease later and console the previous value
+console.log("++a=",++a);  //pre increment will increase first and console the value
+console.log("--a=",--a);  //pre and decrement will decrease first and console the value
