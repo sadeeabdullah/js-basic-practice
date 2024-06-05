6763b820655701ac021c9d5c1608a97f117c1f41
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-06-06 04:11:45 +0600

    added description for loop

--- loops/dowhile_loop.js
+++ loops/dowhile_loop.js
@@ -3,3 +3,4 @@ let i=0;
 do{
     console.log(namelist[i].toUpperCase());
     i++;}while(i<namelist.length);0
+// here first it will run the block and then it will check the condition
--- loops/while_lop.js
+++ loops/while_lop.js
@@ -4,3 +4,4 @@ while(i<namelist.length){
     console.log(namelist[i].toUpperCase());
     i++;
 }
+// first it will check the condition and run the block
