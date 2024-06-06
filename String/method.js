1a72b1e8fdf15a424193fa8d91d09dc220fe553b
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-06-06 07:13:16 +0600

    added the slice method for string

--- String/method.js
+++ String/method.js
@@ -4,3 +4,8 @@ let name = 'sadee'
 
 console.log(name.toUpperCase())
 console.log(name.toLowerCase())
+
+console.log(name.slice(2, 5)) // extracts from index 2 to index 5 (excluding index 5).
+console.log(name.slice(-4)) // extracts the last four characters of the string.
+console.log(name.slice(2))  //extracts from index 2 to the end of the string.
+console.log(name.slice(1,3))  //extracts the character at index 2 and 3.
---
+++ functions/basic.js
@@ -0,0 +1,4 @@
+function namee(a){
+    console.log("hello world! "+a)
+}
+namee("man")
