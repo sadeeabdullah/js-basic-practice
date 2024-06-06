be010a01972704c9ea1b7818f9e882ecaaacd7f6
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-06-06 07:28:02 +0600

    added some methods for string and updated the basic text

--- String/method.js
+++ String/method.js
@@ -9,3 +9,19 @@ console.log(name.slice(2, 5)) // extracts from index 2 to index 5 (excluding ind
 console.log(name.slice(-4)) // extracts the last four characters of the string.
 console.log(name.slice(2))  //extracts from index 2 to the end of the string.
 console.log(name.slice(1,3))  //extracts the character at index 2 and 3.
+
+
+// /replace method
+
+console.log(name.replace('s','v'))
+
+//concating two strings
+
+// we can also use + operator to contcat as well
+let friend= "dhollu"
+
+console.log(name.concat("is a friend of ",friend))
+
+//to remove the spaces
+let friend2 = "  nulla    "
+console.log(friend2.trim())
