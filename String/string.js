13dd4b1461283ac70fddfbb2df581c8c8be3f668
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-06-06 06:36:34 +0600

    added string basics

---
+++ String/string.js
@@ -0,0 +1,13 @@
+let name = "sadee"
+ 
+//string can be declared into single or double quotes
+
+// to get the length of the string
+
+console.log(name.length)
+
+// to get the index of the string
+console.log(name.indexOf("e"))
+
+//or you can the get the value using the index
+console.log(name[4])
