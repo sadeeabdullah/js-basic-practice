8122c78a57da13d3da2ed71a3f30d6e697f7dc2e
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-06-06 06:15:53 +0600

    added the 3rd q

--- practice_set/chapter3.js
+++ practice_set/chapter3.js
@@ -1,4 +1,4 @@
-// write a program to print the marks of a student in an object using for loop
+// Q1 : write a program to print the marks of a student in an object using for loop
 
 let sadeesMark={
     math:34,
@@ -11,10 +11,20 @@ for(let i=0; i<Object.keys(sadeesMark).length; i++){  //here used object.keys() 
     console.log("the marks of"+Object.keys(sadeesMark)[i]+" is" +" " +sadeesMark[Object.keys(sadeesMark)[i]])
 }
 
-//do it using for in loop
+// Q2 : do it using for in loop
 
 for(let key in sadeesMark){
     console.log("the marks of"+ " " + key + " is " + sadeesMark[key])
 }
 
 // its easy and good to use in case of object
+
+//Q3 : write a program to print try again untill    the input is correct number
+
+const theNumber= 221;
+let j = 0;
+
+while(j != theNumber){
+     //console.log("try again")  // i have comment this cause it will run untill the number is correct and if i don't stop it will run infinitly and it will crush the js runtime
+}
+console.log("you have got it right")
