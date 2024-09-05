f2021d2ee926a7fd559a9db886874f517ceffb9e
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-09-05 11:40:57 +0600

    after a long day buddy

---
+++ practice_set/chapter2.js
@@ -0,0 +1,31 @@
+
+let num = "30"
+num =  Number.parseInt(num)
+// switch(num){
+//     case 10:
+//         console.log("his age is 10")
+//         break
+//     case 20:  
+//     console.log(firstname = "sadee")
+//     break
+//     case 30:
+//         console.log("his age is 30")
+//         break   
+//     default:
+//         console.log("his age is unknown")
+// }
+
+// if(num %2 == 0 && num %3 == 0){
+//     console.log("number is divisible by 2 and 3")
+// }
+// else if(num %2 == 0){
+//     console.log("number is divisible by 2")
+// }
+// else if(num %3 == 0){
+//     console.log("number is divisible by 3")
+// }
+// else{
+//     console.log("number is not divisible by 2 or 3")
+// }
+
+console.log(num>18? "eligible" : "not eligible")
