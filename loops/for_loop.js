333db61fae4bbc3db7369ef4b1c6d5d95ab30302
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-09-09 11:39:54 +0600

    updating for loop

--- loops/for_loop.js
+++ loops/for_loop.js
@@ -1,28 +1,4 @@
 const namelist = ["John", "Jane", "Jack", "Jill"];
-for(let i = 0; i<namelist.length; i++){
-    console.log(namelist[i].toUpperCase());
-}
 
-//printing 1000 in js
-
-for( let j = 1; j<1001; j++){
-    console.log(j);
-}
-
-// first n number of numbers summation
-
-let sum = 0;
-let n = 7;
-for( let l = 0; l<n; l++){
-    sum += (l+1) // or sum = sum + (l+1)
-}
-console.log(sum)
-
-// first n number of numbers multiplication or factorial
-
-let mul = 1;
-let m = 7;
-for( let k = 0; k<m; k++){
-    mul *= (k+1) // or mul = mul * (k+1)
-}
-console.log(mul)
+for(i=4;i<101; i= i + 3)
+    console.log(i)
