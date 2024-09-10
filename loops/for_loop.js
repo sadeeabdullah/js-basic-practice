9a5f4f6bef2d00381252c0bfa33bdc62fe353dcf
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-09-10 06:50:40 +0600

    adding program to show the sum of first u natural even number

--- loops/for_loop.js
+++ loops/for_loop.js
@@ -4,12 +4,27 @@
 //     console.log(i)
 
 //  program to add first n natural number
-let sum = 0
-let n = 3
-for (let i = 0 ; i<n;i++){
-    sum += (i + 1)
-    console.log(sum + "+")
-}
-console.log("first" + n + "natural number sum is " + sum)
+// let sum = 0
+// let n = 3
+// for (let i = 0 ; i<n;i++){
+//     sum += (i + 1)
+//     console.log(sum + "+")
+// }
+// console.log("first" + n + "natural number sum is " + sum)
 
 // HERE sum += (i + 1) IS THE SHORTAND OF sum = sum + (i + 1)
+
+
+// let write a sample for loop that add the first u natural even numbers
+
+let total = 0
+let u = 10
+for (let i = 0; i<u; i++){
+   
+        if (i %2 == 0){
+            console.log(i)
+            total += i
+        }
+      
+}
+console.log("first" + u + "natural number sum is " + total)
