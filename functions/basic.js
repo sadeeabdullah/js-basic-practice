cbd229b07d9a132b77d759e33036b39548f4ff98
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-09-11 10:13:11 +0600

    adding function practices

--- functions/basic.js
+++ functions/basic.js
@@ -2,3 +2,13 @@ function namee(a){
     console.log("hello world! "+a)
 }
 namee("man")
+
+//LETS FIND SOME ANOTEHR EXAMPLE OF FUNCTION
+
+function summation(a,b,c){
+    const sum = a+b+c;
+    return sum;
+}
+console.log(summation(1,5,7))
+
+// consol(sum)
