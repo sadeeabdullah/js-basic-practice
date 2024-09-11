75b5cb42af61f1cc284ca2303209bad9a067feed
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-09-12 03:53:09 +0600

    using template literal to add string

--- String/string.js
+++ String/string.js
@@ -2,4 +2,13 @@
 
 let name = "sadee"
 
+//TOO GET THE LENGTH  OF STRING
 console.log(name.length)
+
+//TO GET THE INDEX VALUE OF STRING
+console.log(name[0])
+
+//USING TEMPLATE LITERAL TO ADD A STRING TO ANOTHER
+let friend1 = 'habib'
+let friend2 = 'habil'
+console.log(`${friend1} is a friend of ${friend2}`)
