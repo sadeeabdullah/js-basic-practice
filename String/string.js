61880570967cdd7a71c6e5801beda8c0a1c5de39
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-09-12 04:04:35 +0600

    adding escape character

--- String/string.js
+++ String/string.js
@@ -11,4 +11,13 @@ console.log(name[0])
 //USING TEMPLATE LITERAL TO ADD A STRING TO ANOTHER
 let friend1 = 'habib'
 let friend2 = 'habil'
-console.log(`${friend1} is a friend of ${friend2}`)
+console.log(`${friend1} is a friend of ${friend2}`)  //NOTE : We can use single or double quote into the back tik and surprisingly it will work properly
+console.log(`${friend1} is "a" friend of ${friend2}`) // SAMPLE: habib is "a" friend of habil (It will show the quote into the string)
+
+//LET EXPLORE ESCAPE SEQUENCE CHARACTER
+const abouts = "sadee is a \'gand\' what do you think"
+                    // OR
+const about ="sadee is a 'gand' what do you think"
+
+const slashN = "sadee is a good boy. \n He work so hard"
+console.log(about, abouts, slashN)
