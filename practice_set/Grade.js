ed658bf63993ad8bcbbb9915964921aa366c7b3d
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-10-08 04:45:58 +0600

    added a practice set to get the grade using js

---
+++ practice_set/Grade.js
@@ -0,0 +1,17 @@
+const number = 45;
+
+if(number<=50 && number>33){
+    console.log('you have got grade c');
+}
+else if(number<=60 && number>50){
+    console.log('you have got grade b');
+}
+else if(number<=70 && number>60){
+    console.log('you have got grade A-');
+}
+else if(number<=80 && number>70){
+    console.log('you have got grade A+');
+}
+else {
+    console.log('you are fail');
+}
