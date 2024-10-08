97512f2f7991c21433fed0b9dde95d92a3583d18
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-10-08 06:15:55 +0600

    adding doubleLogicalNot operator uses in js

---
+++ Operators/doubleLogicalNot.js
@@ -0,0 +1,5 @@
+const userId = 10;
+console.log(!!userId) // to get the is the value is available or not. the first logical not will will reverse the value and check and the left logical not will reverse the first value after getting the first logical not using value
+
+console.log(!userId) // singe check if the value is availble or not
+console.log(userId) // check the value
