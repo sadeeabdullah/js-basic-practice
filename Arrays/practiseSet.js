ee7bc433591073c9c1ee5b7290a6cd28daf147ea
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-10-13 06:43:06 +0600

    task 1 and 2 added

--- Arrays/practiseSet.js
+++ Arrays/practiseSet.js
@@ -1,5 +1,5 @@
 
-//REVERSING ARRAY
+//REVERSING ARRAY WITHOUT THE  REVERSE METHOD : TASK 1
 
 let reversedArray = [];
 
@@ -10,3 +10,15 @@ for (const friend of friends) {
     reversedArray.unshift(friend)
 }
 console.log(reversedArray)
+
+
+//Write a JavaScript code to get the even numbers from an array using any looping technique. : TASK 2
+
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+const evenNumber = [];
+for (const n in numbers) {
+     if(numbers[n] % 2 == 0){
+        evenNumber.push(numbers[n])
+     }
+}
+console.log(evenNumber)
