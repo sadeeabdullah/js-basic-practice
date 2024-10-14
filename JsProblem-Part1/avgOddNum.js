fdd479579e482703b8d463fbacdd87104087e50d
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-10-14 08:33:06 +0600

    added the function for average of odd number in array

--- JsProblem-Part1/avgOddNum.js
+++ JsProblem-Part1/avgOddNum.js
@@ -1,13 +1,18 @@
 function avgOdd(numbers) {
 
     let oddNumbers = [];
+    let sumOfOdd = 0;
+    let totalOddCount = 0;
 
     for (const number of numbers) {
         if(number % 2 != 0){
             oddNumbers.push(number)
+            totalOddCount ++;
+            sumOfOdd += number
         }
     }
-    return oddNumbers;
+    const avgOfOdd = sumOfOdd/totalOddCount
+    return [oddNumbers , avgOfOdd];
 }
 console.log(avgOdd([3,12,19,21,33]))
 
