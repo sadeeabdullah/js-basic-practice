469ce95e98dd2b6e01fb0ab5b8d7637402311851
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-10-15 10:10:03 +0600

    adding someo more method for math

--- JsProblem-Part1/math.js
+++ JsProblem-Part1/math.js
@@ -3,3 +3,13 @@
 const min = Math.max(45,451,5454,21564,231,-515,465)
 const max = Math.min(45,451,5454,21564,231,-515,465)
 console.log(min,max)
+
+
+                    /// *** Method for Math object *** ///
+
+console.log(Math.abs(5-475)) // it will provide a absolute value like if the output is negative it will remove or don't  show the sign for negative it will just show the gap  between the numbers
+console.log(Math.PI) // it will show the value of PI. we can use it to get the circle measurements
+console.log(Math.round(2.25)) // it will round the floating number to the nearest integer output: 2
+console.log(Math.round(2.85)) // it will round the floating number to the nearest integer output: 3
+console.log(Math.floor(2.99)) // it will make the lowest integer nearer to the float number. Output: 2
+console.log(Math.ceil(2.99)) // it will make the highest integer nearer to the float number. output:3
