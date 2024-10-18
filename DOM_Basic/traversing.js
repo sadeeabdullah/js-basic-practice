b6c89b30edce1fcfaefff46d74470c1a6e0a3c91
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-10-18 19:48:17 +0600

    using getattribute  and classlist

--- DOM_Basic/traversing.js
+++ DOM_Basic/traversing.js
@@ -17,6 +17,9 @@ for (const text of traverseUsingTheclass) {
         console.log(text.innerText)
 }
 
+// it will only traverse first element
+const singleSelector = document.querySelector(".li-container li")
+console.log(singleSelector)
 
 // The method above will provide HTML collection. We can use forOf into it.
 
@@ -28,9 +31,16 @@ for (const text of traverseUsingTheclass) {
 const multipleSelector = document.querySelectorAll('.li-container li') ;
 console.log(multipleSelector)
 
-// it will only traverse first element
-const singleSelector = document.querySelector(".li-container li")
-console.log(singleSelector)
+
+// if we want to get the other classes of the traversing element then we can use the get attribute
+const att = traverseUsingTheclass[0].getAttribute('class')
+console.log(att)
+// or we can easily get the class using classlist
+console.log(traverseUsingTheclass[0].classList)
+//we can add or remove class using add and remove methods
+traverseUsingTheclass[0].classList.add('w-24')
+traverseUsingTheclass[0].classList.remove('bg-red')
+console.log(traverseUsingTheclass[0].classList)
 
 
 // how to change the style of the html element or building block
