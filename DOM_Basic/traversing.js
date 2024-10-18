a565b104a8a873224f45b454096415ffe755e593
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-10-18 17:27:26 +0600

    adding the selection using the method of id and display the innertext

--- DOM_Basic/traversing.js
+++ DOM_Basic/traversing.js
@@ -1,6 +1,11 @@
 // traversing in programming language is a datastructure signinfies accessing each element in data structure systematically.
 
 
-const allPTag = document.getElementsByTagName('p')
 ///it is a method that select all the P tag from the dom
+const allPTag = document.getElementsByTagName('p')
 console.log(allPTag)
+
+// it is a method that select the portion or div using the ID name and innertext
+const traverseUsingTheId = document.getElementById('title')
+console.log(traverseUsingTheId)
+console.log(traverseUsingTheId.innerText)
