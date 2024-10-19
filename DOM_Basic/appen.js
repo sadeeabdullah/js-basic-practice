5944aea58c0328fd23f6a817a0dfd113f6d729fb
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-10-19 10:43:39 +0600

    adding how to add a new section using appenchild and create element

--- DOM_Basic/appen.js
+++ DOM_Basic/appen.js
@@ -1,5 +1,30 @@
 const li__container = document.getElementById('container')
 const li = document.createElement('li')
 li.innerText="list added from appenjs"
+li.style.color='red'
 li__container.appendChild(li)
 console.log(li__container)
+
+
+
+// how to append a whole section into main
+
+//first traverse the section where you want to appenchild
+const mainSection = document.getElementById('main-container')
+console.log(mainSection)
+
+//creating elements using create element methods
+
+const newSection = document.createElement('section')
+
+const ul = document.createElement('ul')
+const header = document.createElement('h1')
+header.innerText="adding this from appen.js"
+const li1 = document.createElement('li')
+li1.innerText = 'adding into the new section'
+ul.appendChild(li1)
+
+newSection.appendChild(header)
+newSection.appendChild(ul)
+mainSection.appendChild(newSection)
+
