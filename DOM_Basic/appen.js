8080b39b351d61bbc7e9228d0ef3546ea5c4c8d8
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-10-19 10:16:56 +0600

    how to create and appenchild

---
+++ DOM_Basic/appen.js
@@ -0,0 +1,5 @@
+const li__container = document.getElementById('container')
+const li = document.createElement('li')
+li.innerText="list added from appenjs"
+li__container.appendChild(li)
+console.log(li__container)
