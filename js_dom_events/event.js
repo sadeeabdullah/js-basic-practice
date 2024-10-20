d4e529c7b69266fd8c9edef566520c1fa9ae9b03
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-10-20 18:52:15 +0600

    adding how to use the onclick function from the js file effectively

--- js_dom_events/event.js
+++ js_dom_events/event.js
@@ -1,1 +1,6 @@
-console.log('connected')
+const makeButtonBlue = document.getElementById('make-blue'); //traversing the button
+makeButtonBlue.onclick = makeBlue; // here i am just mentioning the function name because if i invoke the funciton it will auto run
+
+function makeBlue() {
+    document.body.style.backgroundColor='blue'
+}
