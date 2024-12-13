33965a8cda8d526966370fa097f1e31f53c16fc7
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-12-13 06:41:52 +0600

    adding destructuring method in the note

---
+++ Object/destructuring.js
@@ -0,0 +1,17 @@
+const human  = {
+    name: "sadee",
+    age: 17,
+    phone : '016682451146',
+    money: 12431232
+}
+
+//  we can get the value normally in this way:
+const name = human.name;
+console.log(name)
+
+// here is the way you can get the value of the property more easily and it is called destructuring.
+// You have use the property name as constant name into a braces and write down the object name on the right side of the  equal
+const {phone,money}= human
+console.log(phone,money)
+
+// NOTE: The plus is you can get multiple value in one line code
